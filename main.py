3d5910882aecc3d6fc8ec7cb1e8816522bc1bdad
andrii <andrijkorolevic@gmail.com>
2022-10-08 10:39:46 +0300

    Created new feature/l_e_2

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("dEv")
+print("Enter a number")
+num = int(input("->"))
+
+try:
+    f = 1
+    for i in range(1, num + 1):
+        f = f * i
+    print(f)
+
+except Exception as ex:
+    print(f'Error', {ex})
