64ce82006df1966e8fad25d35cf828f5a44aa48b
andrii <andrijkorolevic@gmail.com>
2022-10-08 10:08:57 +0300

    Created new feature

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("dEv")
+print("1 number")
+n1 = int(input("->"))
+print("2 number")
+n2 = int(input("->"))
+
+try:
+    f = 0
+
+    for i in range(n1, n2+1):
+        f = f + i
+    print(f)
+
+    for m in range(n1, n2+1):
+        ges = f / i
+        print(ges)
+        break
+
+except Exception as ex:
+    print('Error:', ex)
